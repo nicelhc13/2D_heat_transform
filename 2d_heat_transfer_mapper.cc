9606bfeeb0d028ffe8197b7909bbc5ba6efd46dd
Hochan Lee <hochan@utexas.edu>
2022-06-14 15:54:59 -0500

    Addcomments onto the mapper for heat transfer app

--- 2d_heat_transfer_mapper.cc
+++ 2d_heat_transfer_mapper.cc
@@ -32,21 +32,16 @@ public:
                                     std::vector<Processor> &target_procs);
 
   /*
-  virtual const std::map<VariantID, Processor::Kind>& find_task_variants(
-                                                MapperContext ctx, TaskID task_id);
-                                                */
-
 	virtual void map_constrained_requirement(MapperContext ctx,
 			const RegionRequirement &req, MappingKind mapping_kind,
 			const std::vector<LayoutConstraintID> &constraints,
 			std::vector<PhysicalInstance> &chosen_instances, Processor restricted);
 
-
-
 	virtual void map_random_requirement(MapperContext ctx,
     																  const RegionRequirement &req,
     												 					std::vector<PhysicalInstance> &chosen_instances,
 														 					Processor restricted);
+  */
 
 private:
   std::vector<Processor>& procs_list;
@@ -57,59 +52,37 @@ HeatTransferMapper::HeatTransferMapper(MapperRuntime *rt, Machine machine,
                                        Processor local, const char *mapper_name, 
                                        std::vector<Processor>* _procs_list)
   : DefaultMapper(rt, machine, local, mapper_name), procs_list(*_procs_list) {
-  std::cout << "Constructor \n";
+  std::cout << "[HeatTransferMapper] Constructor \n";
 }
 
 Processor HeatTransferMapper::default_policy_select_initial_processor(
                                     MapperContext ctx, const Task &task)
 {
-  std::cout << "Default policy select initial processor \n";
+  std::cout << "[HeatTransferMapper] Default policy select initial processor \n";
+  // Select the initial processor. First, it finds a compatible variant
+  // and second, it returns the next processor to scheduled the task
+  // in a round-robin manner to give load balancing.
+  // Target processor of each task is getting from the initial processor.
+  // (last processor -> next target processor of the region if nothing is specified)
   return DefaultMapper::default_policy_select_initial_processor(ctx, task);
 }
 
 void HeatTransferMapper::select_task_options(const MapperContext ctx,
                                              const Task& task,
                                              TaskOptions& output) {
-  std::cout << "Select task options \n";
+  std::cout << "[HeatTransferMapper] Select task options \n";
   output.initial_proc = default_policy_select_initial_processor(ctx, task);
+  // An inline task means that a subtask runs on a physical region of
+  // the parent task and requires dependencies between them, which could
+  // lose parallelism.
   output.inline_task = false;
   output.stealable = false;
-  output.map_locally = false;
+  // Enable to use a local mapper or a mapper of the processor to be scheduled.
+  output.map_locally = true;
 }
 
-/*
-const std::map<VariantID, Processor::Kind>& HeatTransferMapper::find_task_variants(
-                                                MapperContext ctx, TaskID task_id) {
-  std::map<TaskID, std::map<VariantID, Processor::Kind>> variant_processor_kinds;
-  std::map<TaskID, std::map<VariantID, Processor::Kind>>::const_iterator
-    finder = variant_processor_kinds.find(task_id);
-  if (finder != variant_processor_kinds.end()) {
-    return finder->second;
-  }
-  std::vector<VariantID> valid_variants;
-  runtime->find_valid_variants(ctx, task_id, valid_variants);
-
-	std::cout << "Task ID:" << task_id << "\n";
-	std::cout << "Valid variant size:" << valid_variants.size() << "\n";
-  std::map<VariantID, Processor::Kind> kinds;
-  for (std::vector<VariantID>::const_iterator it = valid_variants.begin();
-        it != valid_variants.end(); ++it) {
-    const ExecutionConstraintSet &constraints =
-      runtime->find_execution_constraints(ctx, task_id, *it);
-    if (constraints.processor_constraint.is_valid())
-      kinds[*it] = constraints.processor_constraint.valid_kinds[0];
-    else
-      kinds[*it] = Processor::LOC_PROC;
-  }
-  std::map<VariantID, Processor::Kind> &result =
-    variant_processor_kinds[task_id];
-  result = kinds;
-	std::cout << "Valid variant size:" << valid_variants.size() << " [done] \n";
-  return result;
-}
-*/
-
-void HeatTransferMapper::map_constrained_requirement(MapperContext ctx,
+#if 0
+void map_constrained_requirement(MapperContext ctx,
     const RegionRequirement &req, MappingKind mapping_kind,
     const std::vector<LayoutConstraintID> &constraints,
     std::vector<PhysicalInstance> &chosen_instances, Processor restricted) {
@@ -120,13 +93,16 @@ void HeatTransferMapper::map_constrained_requirement(MapperContext ctx,
     const LayoutConstraintSet &layout_constraints =
       runtime->find_layout_constraints(ctx, *lay_it);
     Machine::MemoryQuery all_memories(machine);
-    if (restricted.exists())
+    // Want to restrict candidate processors if specified.
+    if (restricted.exists()) {
+      // If processors are restricted, find memory that is affinite to them.
       all_memories.has_affinity_to(restricted);
+    }
+
     // This could be a big data structure in a big machine
     std::map<unsigned,Memory> random_memories;
     for (Machine::MemoryQuery::iterator it = all_memories.begin();
-          it != all_memories.end(); it++)
-    {
+          it != all_memories.end(); it++) {
       random_memories[default_generate_random_integer()] = *it;
     }
     bool made_instance = false;
@@ -135,12 +111,15 @@ void HeatTransferMapper::map_constrained_requirement(MapperContext ctx,
       std::map<unsigned,Memory>::iterator it = random_memories.begin();
       Memory target = it->second;
       random_memories.erase(it);
-      if (target.capacity() == 0)
+      if (target.capacity() == 0) {
         continue;
+      }
+      // If it finds a proper memory that has enough free memory
+      // and satisfies layout constraints, create a new physical
+      // region instance of the passed instance.
       if (default_make_instance(ctx, target, layout_constraints,
             chosen_instances[output_idx], mapping_kind,
-            true/*force new*/, false/*meets*/, req))
-      {
+            true/*force new*/, false/*meets*/, req)) {
         made_instance = true;
         break;
       }
@@ -152,25 +131,26 @@ void HeatTransferMapper::map_constrained_requirement(MapperContext ctx,
   }
 }
 
-void HeatTransferMapper::map_random_requirement(MapperContext ctx,
+/// TODO(lhc): does not use it. Just write it as a study.
+/// This function should be called if a region does not have any
+/// constraint. If it has, use map_constrained_requirement().
+void map_random_requirement(MapperContext ctx,
     const RegionRequirement &req,
     std::vector<PhysicalInstance> &chosen_instances, Processor restricted) {
-
   std::vector<LogicalRegion> regions(1, req.region);
   chosen_instances.resize(req.privilege_fields.size());
   unsigned output_idx = 0;
   // Iterate over all the fields and make a separate instance and
-  // put it in random places
+  // put it in random places.
   for (std::set<FieldID>::const_iterator it = req.privilege_fields.begin();
-        it != req.privilege_fields.end(); it++, output_idx++)
-  {
+        it != req.privilege_fields.end(); it++, output_idx++) {
     std::vector<FieldID> field(1, *it);
     // Try a bunch of memories in a random order until we find one 
-    // that succeeds
+    // that succeeds.
     Machine::MemoryQuery all_memories(machine);
     if (restricted.exists())
       all_memories.has_affinity_to(restricted);
-    // This could be a big data structure in a big machine
+    // This could be a big data structure in a big machine.
     std::map<unsigned,Memory> random_memories;
     for (Machine::MemoryQuery::iterator it = all_memories.begin();
           it != all_memories.end(); it++)
@@ -183,8 +163,9 @@ void HeatTransferMapper::map_random_requirement(MapperContext ctx,
       std::map<unsigned,Memory>::iterator it = random_memories.begin();
       Memory target = it->second;
       random_memories.erase(it);
-      if (target.capacity() == 0)
+      if (target.capacity() == 0) {
         continue;
+      }
       // TODO: put in arbitrary constraints to mess with the DMA system
       LayoutConstraintSet constraints;
       default_policy_select_constraints(ctx, constraints, target, req);
@@ -206,27 +187,35 @@ void HeatTransferMapper::map_random_requirement(MapperContext ctx,
     }
   }
 }
+#endif
 
 
 void HeatTransferMapper::map_task(const MapperContext ctx, const Task& task,
                            const MapTaskInput& input, MapTaskOutput& output) {
-  /*
+#if 0
   const std::map<VariantID,Processor::Kind> &variant_kinds =
     find_task_variants(ctx, task.task_id);
   std::vector<VariantID> variants;
-  for (std::map<VariantID,Processor::Kind>::const_iterator it =
+  // Find compatible (same kind of the target processor of the task)
+  // processors' variant ids to the target processor kind.
+  for (std::map<VariantID, Processor::Kind>::const_iterator it =
         variant_kinds.begin(); it != variant_kinds.end(); it++) {
     if (task.target_proc.kind() == it->second)
       variants.push_back(it->first);
   }
+  // Any processor compatible to the task should exist.
   assert(!variants.empty());
   if (variants.size() > 1) {
     int chosen = default_generate_random_integer() % variants.size();
     output.chosen_variant = variants[chosen];
   }
-  else
+  else {
     output.chosen_variant = variants[0];
+  }
   output.target_procs.push_back(task.target_proc);
+
+  // TODO(lhc): find premapped regions. But how can it know if
+  // the region is mapped on to a specific processor? what does it aim for?
   std::vector<bool> premapped(task.regions.size(), false);
   for (unsigned idx = 0; idx < input.premapped_regions.size(); idx++) {
     unsigned index = input.premapped_regions[idx];
@@ -253,10 +242,11 @@ void HeatTransferMapper::map_task(const MapperContext ctx, const Task& task,
       map_constrained_requirement(ctx, task.regions[idx], TASK_MAPPING,
           constraints, output.chosen_instances[idx], task.target_proc);
     }
-    else
+    else {
       map_random_requirement(ctx, task.regions[idx],
                              output.chosen_instances[idx],
                              task.target_proc);
+    }
   }
   output.task_priority = default_generate_random_integer();
 
@@ -266,7 +256,7 @@ void HeatTransferMapper::map_task(const MapperContext ctx, const Task& task,
     output.task_prof_requests.add_measurement<OperationTimeline>();
     output.task_prof_requests.add_measurement<RuntimeOverhead>();
   }
-  */
+#endif
   DefaultMapper::map_task(ctx, task, input, output);
 }
 
@@ -274,7 +264,7 @@ static void create_mappers(Machine machine, Runtime *runtime,
                            const std::set<Processor> &local_procs) {
   std::vector<Processor>* procs_list = new std::vector<Processor>();
 
-  std::cout <<"Create mappers\n";
+  std::cout << "[HeatTransferMapper] Create mappers\n";
 
   // Construct a processor list for a mapper.
   Machine::ProcessorQuery procs_query(machine);
@@ -283,10 +273,10 @@ static void create_mappers(Machine machine, Runtime *runtime,
        it != procs_query.end(); ++it) {
     // Each iterator points to a Processor object.
     procs_list->push_back(*it);
-    std::cout << std::distance(procs_query.begin(), it) <<
-      " -> " << it->get_kind_name(it->kind()) << "\n";
+    std::cout << "Create a mapper on " <<
+                      it->get_kind_name(it->kind()) << "(idx:" <<
+                      std::distance(procs_query.begin(), it) << ")\n";
   }
-
   // Each local processor has a mapper and at the same time, the mapper
   // maintains global processor information.
   for (std::set<Processor>::const_iterator it = local_procs.begin();
